aa7223650edebfc22600a9239da1d0c5b9535afb
Hadrien Grasland <grasland@lal.in2p3.fr>
2023-03-07 16:34:22 +0100

    Implement first chunking iterator, and bring back last which on second examination was actually correct

--- src/vectorize/vectors.rs
+++ src/vectorize/vectors.rs
@@ -16,6 +16,7 @@ use crate::{vectorize::Vectorizable, Vector};
 use core::{
     iter::FusedIterator,
     marker::PhantomData,
+    num::NonZeroUsize,
     ops::{Bound, Range, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive},
 };
 
@@ -153,7 +154,30 @@ impl<V: VectorInfo, Data: VectorizedImpl<V>> Vectors<V, Data> {
         <&mut Self>::into_iter(self)
     }
 
-    // TODO: chunks(_exact)? : mark inline
+    /// Returns an iterator over `chunk_size` elements of the dataset at a time,
+    /// starting at the beginning of the dataset
+    ///
+    /// The chunks are slices and do not overlap. If `chunk_size` does not
+    /// divide the length of the dataset, then the last chunk will not have
+    /// length `chunk_size`.
+    ///
+    /// See [`chunks_exact()`] for a variant of this iterator that returns
+    /// chunks of always exactly `chunk_size` elements, and [`rchunks()`] for
+    /// the same iterator but starting at the end of the slice.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `chunk_size` is 0.
+    #[inline]
+    pub fn chunks(&mut self, chunk_size: usize) -> Chunks<V, Data> {
+        let chunk_size = NonZeroUsize::new(chunk_size).expect("Chunks must have nonzero size");
+        Chunks {
+            remainder: Some(self.as_slice()),
+            chunk_size,
+        }
+    }
+
+    // TODO: chunks_exact : mark inline
 
     /// Returns an iterator over N elements at a time, starting at the
     /// beginning of the container
@@ -508,7 +532,7 @@ macro_rules! impl_iterator {
             type Item = Data::$elem$(<$lifetime>)?;
 
             #[inline(always)]
-            fn next<'iter>(&'iter mut self) -> Option<Self::Item> {
+            fn next(&mut self) -> Option<Self::Item> {
                 if self.start < self.end {
                     self.start += 1;
                     Some(unsafe { self.get_elem(self.start - 1) })
@@ -528,11 +552,17 @@ macro_rules! impl_iterator {
             }
 
             #[inline]
+            fn last(mut self) -> Option<Self::Item> {
+                self.next_back()
+            }
+
+            #[inline]
             fn nth(&mut self, n: usize) -> Option<Self::Item> {
-                if self.start.checked_add(n)? < self.end {
+                if self.len() >= n {
                     self.start += n - 1;
                     self.next()
                 } else {
+                    self.start = self.end;
                     None
                 }
             }
@@ -553,10 +583,11 @@ macro_rules! impl_iterator {
 
             #[inline]
             fn nth_back(&mut self, n: usize) -> Option<Self::Item> {
-                if self.start < self.end.checked_sub(n)? {
+                if self.len() >= n {
                     self.end -= n - 1;
                     self.next_back()
                 } else {
+                    self.end = self.start;
                     None
                 }
             }
@@ -591,6 +622,146 @@ impl_iterator!(
     /// Consuming iterator of [`Vectors`]' elements
     (IntoIter, Data::Element)
 );
+
+/// An iterator over [`Vectors`] in (non-overlapping) chunks (`chunk_size`
+/// elements at a time), starting at the beginning of the dataset
+///
+/// When the dataset length is not evenly divided by the chunk size, the last
+/// slice of the iteration will be the remainder.
+///
+/// This struct is created by [`Vectors::chunks()`]
+pub struct Chunks<'vectors, V: VectorInfo, Data: VectorizedImpl<V> + 'vectors> {
+    remainder: Option<Slice<'vectors, V, Data>>,
+    chunk_size: NonZeroUsize,
+}
+//
+impl<'vectors, V: VectorInfo, Data: VectorizedImpl<V>> Chunks<'vectors, V, Data> {
+    /// Chunk size (guaranteed not to be zero)
+    #[inline(always)]
+    fn chunk_size(&self) -> usize {
+        self.chunk_size.into()
+    }
+}
+//
+impl<'vectors, V: VectorInfo, Data: VectorizedImpl<V>> Iterator for Chunks<'vectors, V, Data> {
+    type Item = Slice<'vectors, V, Data>;
+
+    #[inline(always)]
+    fn next(&mut self) -> Option<Self::Item> {
+        let remainder = self.remainder.take()?;
+        let chunk = if remainder.len() > self.chunk_size() {
+            let (chunk, remainder) = unsafe { remainder.split_at_unchecked(self.chunk_size()) };
+            self.remainder = Some(remainder);
+            chunk
+        } else {
+            remainder
+        };
+        Some(chunk)
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len(), Some(self.len()))
+    }
+
+    #[inline]
+    fn count(self) -> usize {
+        self.len()
+    }
+
+    #[inline]
+    fn last(mut self) -> Option<Self::Item> {
+        self.next_back()
+    }
+
+    #[inline]
+    fn nth(&mut self, n: usize) -> Option<Self::Item> {
+        if self.len() >= n {
+            if n > 0 {
+                let mut remainder = self.remainder.take()?;
+                remainder = unsafe { remainder.split_at_unchecked((n - 1) * self.chunk_size()) }.1;
+                self.remainder = Some(remainder);
+            }
+            self.next()
+        } else {
+            self.remainder = None;
+            None
+        }
+    }
+}
+//
+impl<'vectors, V: VectorInfo, Data: VectorizedImpl<V>> DoubleEndedIterator
+    for Chunks<'vectors, V, Data>
+{
+    #[inline(always)]
+    fn next_back(&mut self) -> Option<Self::Item> {
+        let remainder = self.remainder.take()?;
+        let chunk = if remainder.len() > self.chunk_size() {
+            let last_chunk_len = NonZeroUsize::new(remainder.len() % self.chunk_size())
+                .map(usize::from)
+                .unwrap_or(self.chunk_size());
+            let remainder_len = remainder.len();
+            let (remainder, chunk) =
+                unsafe { remainder.split_at_unchecked(remainder_len - last_chunk_len) };
+            self.remainder = Some(remainder);
+            chunk
+        } else {
+            remainder
+        };
+        Some(chunk)
+    }
+
+    #[inline]
+    fn nth_back(&mut self, mut n: usize) -> Option<Self::Item> {
+        // Handle out-of-bounds n
+        if self.len() < n {
+            self.remainder = None;
+            return None;
+        }
+
+        // Handle oddly sized last chunk
+        let last_chunk = self.next_back()?;
+        if n == 0 {
+            return Some(last_chunk);
+        }
+        n -= 1;
+
+        // Skip extra regular chunks in the back we're not interested in
+        if n > 0 {
+            let mut remainder = self.remainder.take()?;
+            let remainder_len = remainder.len();
+            remainder = unsafe {
+                remainder.split_at_unchecked(remainder_len - (n - 1) * self.chunk_size())
+            }
+            .0;
+            self.remainder = Some(remainder);
+        }
+
+        // The chunk we want is now in the back
+        self.next_back()
+    }
+}
+//
+impl<'vectors, V: VectorInfo, Data: VectorizedImpl<V>> ExactSizeIterator
+    for Chunks<'vectors, V, Data>
+{
+    #[inline]
+    fn len(&self) -> usize {
+        if let Some(remainder) = self.remainder.as_ref() {
+            (remainder.len() / self.chunk_size) + (remainder.len() % self.chunk_size == 0) as usize
+        } else {
+            0
+        }
+    }
+}
+//
+impl<'vectors, V: VectorInfo, Data: VectorizedImpl<V>> FusedIterator for Chunks<'vectors, V, Data> {}
+//
+#[cfg(feature = "iterator_ilp")]
+unsafe impl<'vectors, V: VectorInfo, Data: VectorizedImpl<V>> iterator_ilp::TrustedLowerBound
+    for Chunks<'vectors, V, Data>
+{
+}
 
 /// Slice of [`Vectors`]
 pub type Slice<'a, V, Data> = Vectors<V, <Data as Vectorized<V>>::Slice<'a>>;
