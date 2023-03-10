9ba8dcc54bccd6c98c7c9f0491123dc565363112
Hadrien Grasland <grasland@lal.in2p3.fr>
2023-03-10 16:26:48 +0100

    De-macrofy iterator impl

--- src/vectorize/vectors/iterators/values.rs
+++ src/vectorize/vectors/iterators/values.rs
@@ -3,196 +3,174 @@
 use crate::vectorize::{data::VectorizedImpl, RefSlice, Slice, VectorInfo, Vectors};
 use core::iter::FusedIterator;
 
-/// Read-only [`Vectors`] iterator
-pub struct Iter<'vectors, V: VectorInfo, Data: VectorizedImpl<V>> {
-    vectors: &'vectors Vectors<V, Data>,
-    start: usize,
-    end: usize,
+/// Genericity over Vectors, &Vectors and &mut Vectors
+trait VectorsLike {
+    fn len(&self) -> usize;
+
+    type Item;
+
+    /// Extract the idx-th element under assumption it won't be extracted again
+    ///
+    /// # Safety
+    ///
+    /// - This should only be called once per index, i.e. you must ensure
+    ///   that the iterator will not visit this index again
+    /// - This should only be called for valid indices of the underlying Vectors
+    unsafe fn extract_item(&mut self, idx: usize) -> Self::Item;
 }
 //
-impl<'vectors, V: VectorInfo, Data: VectorizedImpl<V> + 'vectors> IntoIterator
-    for &'vectors Vectors<V, Data>
-{
-    type Item = Data::ElementCopy;
-    type IntoIter = Iter<'vectors, V, Data>;
+impl<V: VectorInfo, Data: VectorizedImpl<V>> VectorsLike for Vectors<V, Data> {
+    fn len(&self) -> usize {
+        Vectors::len(self)
+    }
 
-    #[inline]
-    fn into_iter(self) -> Self::IntoIter {
-        let end = self.len;
-        Self::IntoIter {
-            vectors: self,
-            start: 0,
-            end,
-        }
+    type Item = Data::Element;
+
+    unsafe fn extract_item<'iter>(&'iter mut self, idx: usize) -> Self::Item {
+        debug_assert!(idx < self.len());
+        let result = unsafe { self.get_unchecked_ref(idx) };
+        // This is safe because the VectorizedImpl contract says that ElementRef
+        // should just be a lower-lifetime version of Element.
+        unsafe { core::mem::transmute_copy::<Data::ElementRef<'iter>, Data::Element>(&result) }
     }
 }
+//
+impl<'vectors, V: VectorInfo, Data: VectorizedImpl<V>> VectorsLike for &'vectors Vectors<V, Data> {
+    fn len(&self) -> usize {
+        Vectors::len(self)
+    }
 
-/// In-place [`Vectors`] iterator
-pub struct RefIter<'vectors, V: VectorInfo, Data: VectorizedImpl<V>> {
-    vectors: &'vectors mut Vectors<V, Data>,
-    start: usize,
-    end: usize,
+    type Item = Data::ElementCopy;
+
+    unsafe fn extract_item(&mut self, idx: usize) -> Self::Item {
+        debug_assert!(idx < self.len());
+        unsafe { self.get_unchecked(idx) }
+    }
 }
 //
-impl<'vectors, V: VectorInfo, Data: VectorizedImpl<V> + 'vectors> IntoIterator
+impl<'vectors, V: VectorInfo, Data: VectorizedImpl<V>> VectorsLike
     for &'vectors mut Vectors<V, Data>
 {
-    type Item = Data::ElementRef<'vectors>;
-    type IntoIter = RefIter<'vectors, V, Data>;
+    fn len(&self) -> usize {
+        Vectors::len(self)
+    }
 
-    #[inline]
-    fn into_iter(self) -> Self::IntoIter {
-        let end = self.len;
-        Self::IntoIter {
-            vectors: self,
-            start: 0,
-            end,
+    type Item = Data::ElementRef<'vectors>;
+
+    unsafe fn extract_item<'iter>(&'iter mut self, idx: usize) -> Self::Item {
+        debug_assert!(idx < self.len());
+        let result = unsafe { self.get_unchecked_ref(idx) };
+        // This is safe under the extract_item contract
+        unsafe {
+            core::mem::transmute_copy::<Data::ElementRef<'iter>, Data::ElementRef<'vectors>>(
+                &result,
+            )
         }
     }
 }
 
-/// Consuming [`Vectors`] iterator
-pub struct IntoIter<V: VectorInfo, Data: VectorizedImpl<V>> {
-    vectors: Vectors<V, Data>,
+/// Iterator over owned, copied or borrowed [`Vectors`] elements
+pub struct GenericIter<Vecs: VectorsLike> {
+    vectors: Vecs,
     start: usize,
     end: usize,
 }
 //
-impl<'vectors, V: VectorInfo, Data: VectorizedImpl<V> + 'vectors> IntoIterator
-    for Vectors<V, Data>
-{
-    type Item = Data::Element;
-    type IntoIter = IntoIter<V, Data>;
-
-    #[inline]
-    fn into_iter(self) -> Self::IntoIter {
-        let end = self.len;
-        Self::IntoIter {
-            vectors: self,
+impl<Vecs: VectorsLike> GenericIter<Vecs> {
+    /// Start iteration
+    fn new(vectors: Vecs) -> Self {
+        let end = vectors.len();
+        Self {
+            vectors,
             start: 0,
             end,
         }
     }
 }
+//
+impl<Vecs: VectorsLike> Iterator for GenericIter<Vecs> {
+    type Item = Vecs::Item;
 
-// Common Iterator implementation boilerplate
-macro_rules! impl_iterator {
-    (
-        ($name:ident$(<$iter_lifetime:lifetime>)?, $get_unchecked:ident, $slice:ident, Data::$elem:ident$(<$elem_lifetime:lifetime>)?)
-    ) => {
-        impl<$($iter_lifetime,)? V: VectorInfo, Data: VectorizedImpl<V>> $name<$($iter_lifetime,)? V, Data> {
-            /// Get the i-th element
-            ///
-            /// # Safety
-            ///
-            /// - This should only be called once per index, i.e. you must ensure
-            ///   that the iterator will not visit this index again.
-            /// - This should only be called for valid indices of the underlying Vectors.
-            #[inline(always)]
-            unsafe fn get_elem<'iter>(&'iter mut self, idx: usize) -> Data::$elem$(<$elem_lifetime>)? {
-                debug_assert!(idx < self.vectors.len());
-                let result = unsafe { self.vectors.$get_unchecked(idx) };
-                unsafe { core::mem::transmute_copy(&result) }
-            }
+    #[inline(always)]
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.start < self.end {
+            self.start += 1;
+            Some(unsafe { self.vectors.extract_item(self.start - 1) })
+        } else {
+            None
         }
-        //
-        impl<$($iter_lifetime,)? V: VectorInfo, Data: VectorizedImpl<V>> Iterator
-            for $name<$($iter_lifetime,)? V, Data>
-        {
-            type Item = Data::$elem$(<$elem_lifetime>)?;
+    }
 
-            #[inline(always)]
-            fn next(&mut self) -> Option<Self::Item> {
-                if self.start < self.end {
-                    self.start += 1;
-                    Some(unsafe { self.get_elem(self.start - 1) })
-                } else {
-                    None
-                }
-            }
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len(), Some(self.len()))
+    }
 
-            #[inline]
-            fn size_hint(&self) -> (usize, Option<usize>) {
-                (self.len(), Some(self.len()))
-            }
+    #[inline]
+    fn count(self) -> usize {
+        self.len()
+    }
 
-            #[inline]
-            fn count(self) -> usize {
-                self.len()
-            }
+    #[inline]
+    fn last(mut self) -> Option<Self::Item> {
+        self.next_back()
+    }
 
-            #[inline]
-            fn last(mut self) -> Option<Self::Item> {
-                self.next_back()
-            }
-
-            #[inline]
-            fn nth(&mut self, n: usize) -> Option<Self::Item> {
-                if self.len() >= n {
-                    self.start += n - 1;
-                    self.next()
-                } else {
-                    self.start = self.end;
-                    None
-                }
-            }
-        }
-        //
-        impl<$($iter_lifetime,)? V: VectorInfo, Data: VectorizedImpl<V>> DoubleEndedIterator
-            for $name<$($iter_lifetime,)? V, Data>
-        {
-            #[inline(always)]
-            fn next_back(&mut self) -> Option<Self::Item> {
-                if self.start < self.end {
-                    self.end -= 1;
-                    Some(unsafe { self.get_elem(self.end + 1) })
-                } else {
-                    None
-                }
-            }
-
-            #[inline]
-            fn nth_back(&mut self, n: usize) -> Option<Self::Item> {
-                if self.len() >= n {
-                    self.end -= n - 1;
-                    self.next_back()
-                } else {
-                    self.end = self.start;
-                    None
-                }
-            }
-        }
-        //
-        impl<$($iter_lifetime,)? V: VectorInfo, Data: VectorizedImpl<V>> ExactSizeIterator
-            for $name<$($iter_lifetime,)? V, Data>
-        {
-            #[inline]
-            fn len(&self) -> usize {
-                self.end - self.start
-            }
-        }
-        //
-        impl<$($iter_lifetime,)? V: VectorInfo, Data: VectorizedImpl<V>> FusedIterator
-            for $name<$($iter_lifetime,)? V, Data>
-        {
-        }
-        //
-        #[cfg(feature = "iterator_ilp")]
-        unsafe impl<$($iter_lifetime,)? V: VectorInfo, Data: VectorizedImpl<V>> iterator_ilp::TrustedLowerBound
-            for $name<$($iter_lifetime,)? V, Data>
-        {
+    #[inline]
+    fn nth(&mut self, n: usize) -> Option<Self::Item> {
+        if self.len() >= n {
+            self.start += n - 1;
+            self.next()
+        } else {
+            self.start = self.end;
+            None
         }
     }
 }
 //
-impl_iterator!((Iter<'vectors>, get_unchecked, Slice, Data::ElementCopy));
-impl_iterator!((
-    RefIter<'vectors>,
-    get_unchecked_ref,
-    SliceMut,
-    Data::ElementRef<'vectors>
-));
-impl_iterator!((IntoIter, get_unchecked_ref, SliceMut, Data::Element));
+impl<Vecs: VectorsLike> DoubleEndedIterator for GenericIter<Vecs> {
+    #[inline(always)]
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.start < self.end {
+            self.end -= 1;
+            Some(unsafe { self.vectors.extract_item(self.end + 1) })
+        } else {
+            None
+        }
+    }
+
+    #[inline]
+    fn nth_back(&mut self, n: usize) -> Option<Self::Item> {
+        if self.len() >= n {
+            self.end -= n - 1;
+            self.next_back()
+        } else {
+            self.end = self.start;
+            None
+        }
+    }
+}
+//
+impl<Vecs: VectorsLike> ExactSizeIterator for GenericIter<Vecs> {
+    #[inline]
+    fn len(&self) -> usize {
+        self.end - self.start
+    }
+}
+//
+impl<Vecs: VectorsLike> FusedIterator for GenericIter<Vecs> {}
+//
+#[cfg(feature = "iterator_ilp")]
+unsafe impl<Vecs: VectorsLike> iterator_ilp::TrustedLowerBound for GenericIter<Vecs> {}
+
+/// Read-only [`Vectors`] iterator
+pub type Iter<'vectors, V, Data> = GenericIter<&'vectors Vectors<V, Data>>;
+
+/// In-place [`Vectors`] iterator
+pub type RefIter<'vectors, V, Data> = GenericIter<&'vectors mut Vectors<V, Data>>;
+
+/// Consuming [`Vectors`] iterator
+pub type IntoIter<V, Data> = GenericIter<Vectors<V, Data>>;
 
 // Conversion to slice is iterator type specific
 impl<'vectors, V: VectorInfo, Data: VectorizedImpl<V>> Iter<'vectors, V, Data> {
@@ -238,6 +216,39 @@ impl<V: VectorInfo, Data: VectorizedImpl<V>> IntoIter<V, Data> {
     #[inline]
     pub fn as_ref_slice(&mut self) -> RefSlice<V, Data> {
         unsafe { self.vectors.get_unchecked_ref(self.start..self.end) }
+    }
+}
+
+/// IntoIterator implementation can't be generic over Vecs because orphan rules don't allow it
+impl<V: VectorInfo, Data: VectorizedImpl<V>> IntoIterator for Vectors<V, Data> {
+    type Item = Data::Element;
+    type IntoIter = IntoIter<V, Data>;
+
+    #[inline]
+    fn into_iter(self) -> Self::IntoIter {
+        Self::IntoIter::new(self)
+    }
+}
+//
+impl<'vectors, V: VectorInfo, Data: VectorizedImpl<V>> IntoIterator for &'vectors Vectors<V, Data> {
+    type Item = Data::ElementCopy;
+    type IntoIter = Iter<'vectors, V, Data>;
+
+    #[inline]
+    fn into_iter(self) -> Self::IntoIter {
+        Self::IntoIter::new(self)
+    }
+}
+//
+impl<'vectors, V: VectorInfo, Data: VectorizedImpl<V>> IntoIterator
+    for &'vectors mut Vectors<V, Data>
+{
+    type Item = Data::ElementRef<'vectors>;
+    type IntoIter = RefIter<'vectors, V, Data>;
+
+    #[inline]
+    fn into_iter(self) -> Self::IntoIter {
+        Self::IntoIter::new(self)
     }
 }
 
